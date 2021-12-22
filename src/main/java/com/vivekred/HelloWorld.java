c8b7dacca38c08be3d03a9005fc15d87368637c0
viv_tiw <yt4fun2@gmail.com>
2021-12-22 23:27:36 +0530

    commit no 1;

--- src/main/java/com/vivekred/HelloWorld.java
+++ src/main/java/com/vivekred/HelloWorld.java
@@ -1,2 +1,24 @@
-package com.vivekred;public class HelloWorld {
+package com.vivekred;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class HelloWorld {
+
+    public static void main(String []args) throws Exception {
+        Connection con = new DBUtil().getConnection();
+
+        String sql = "select * from pet";
+
+       Statement st =  con.createStatement();
+       ResultSet rs = st.executeQuery(sql);
+
+       while(rs.next()){
+           System.out.println(rs.getString(1));
+       }
+
+    }
+
 }
